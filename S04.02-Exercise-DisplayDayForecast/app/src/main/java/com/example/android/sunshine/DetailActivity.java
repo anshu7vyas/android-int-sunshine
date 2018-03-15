670eedfd902f934a2ad91681ec31b5cf7249d107
av-7 <amanshulvyas@gmail.com>
2018-03-14 19:13:43 -0700

    completes S04.02 Display Details in Another Activity

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,34 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private TextView mWeatherDisplay;
+    String mForecast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_detail_weather_data);
+
+        // DONE (2) Display the weather forecast that was passed from MainActivity
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+
+
+
     }
 }
